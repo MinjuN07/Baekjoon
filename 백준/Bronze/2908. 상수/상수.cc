1a6388574ff9df69b949aadf249dcd328f73c80d
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2023-10-14 23:15:53 +0900

    [Bronze II] Title: 상수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.cc
+++ 백준/Bronze/2908. 상수/상수.cc
@@ -1,17 +1,10 @@
 #include <stdio.h>
 
 int main(void) {
-		int a1,a2,a3,b1,b2,b3;
-		scanf("%1d%1d%1d %1d%1d%1d",&a1,&a2,&a3,&b1,&b2,&b3);
-		if(a3 > b3) printf("%d%d%d",a3,a2,a1);
-		else if(a3 == b3) {
-			if(a2 > b2) printf("%d%d%d",a3,a2,a1);
-			else if(a2 == b2) {
-				if(a1 >= b1) printf("%d%d%d",a3,a2,a1);
-				else printf("%d%d%d",b3,b2,b1);
-			}
-			else printf("%d%d%d",b3,b2,b1);
-		}
-		else printf("%d%d%d",b3,b2,b1);
-	return 0;
+		int a,b;
+		scanf("%d %d",&a,&b);
+		a = a%10*100 + a%100/10*10 + a/100;
+		b = b%10*100 + b%100/10*10 + b/100;
+		printf("%d",a>=b?a:b);
+		return 0;
 }

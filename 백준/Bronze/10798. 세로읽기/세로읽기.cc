5ad2145c6e6e2595c43f531f3a6bd1f35252b7fe
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2023-11-14 09:30:25 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.cc
+++ 백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -1,14 +1,11 @@
 #include <stdio.h>
-char a[6][16] = {0};
+char a[6][16];
 int main(){
 	int j,i;
-	for(i=0;i < 5;i++){
-		scanf("%s",a[i]);
-	}
-	for(j = 0;j < 15;j++){
-		for(i = 0;i < 5;i++){
-			if(a[i][j] == '\0') continue;
-			printf("%c",a[i][j]);
+	for(i=0;i<5;i++) scanf("%s",a[i]);
+	for(j=0;j<15;j++){
+		for(i=0;i<5;i++){
+			if(a[i][j] != '\0') printf("%c",a[i][j]);
 		}
 	}
 	return 0;

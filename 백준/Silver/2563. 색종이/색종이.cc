c7904ee7590c63d965781b29dfb5d0cb05a8a31a
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2023-11-14 17:42:05 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 1152 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -1,15 +1,11 @@
 #include<stdio.h>
 int arr[101][101];
 int main(){
-	int n,a,b,i,j,k,gum=0;
+	int n,a,b,i,j,sum=0;
 	scanf("%d",&n);
-	for(k=0;k<n;k++){
-		int sum=0;
-		scanf("%d %d",&a,&b);
+	while(scanf("%d %d",&a,&b)!=EOF){
 		for(i=a;i<a+10;i++){
 			for(j=b;j<b+10;j++){
-				if(arr[i][j]) sum++;
-				arr[i][j] = 1;}}
-		gum+=100-sum;}
-	printf("%d",gum);
+				if(arr[i][j]!=1)sum+=arr[i][j]=1;}}}
+	printf("%d",sum);
 }

14db8486803282ecbfb058daa94054527db9d2d3
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2023-11-19 23:04:58 +0900

    [Bronze III] Title: 배수와 약수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.cc
@@ -1,10 +1,7 @@
 #include<stdio.h>
 int main(){
-	int n,m,f;
-	while(scanf("%d %d",&n,&m)&&(n+m!=0)){
-		f=0;
-		if(m%n==0)f=2;
-		if(n%m==0)f=1;
-		printf("%s\n",(f>1?"factor":((!f)?"neither":"multiple")));
-	}
+    int n,m;
+    for(;scanf("%d %d",&n,&m),n+m!=0;){
+        printf("%s\n",m%n==0?"factor":(n%m==0?"multiple":"neither"));
+    }
 }

a358adde6ddf9ec3c6661943da132cfc0cbe62a4
2minJun <127070837+MinjuN07@users.noreply.github.com>
2024-02-09 14:48:07 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.cc
@@ -1,8 +1,8 @@
 #include<stdio.h>
-long long a[91]={0,1,1,2,3,5,8,13,21,34,55};
+long long a[91]={0,1};
 long long f(int x){
-	if(!x) return 0;
-	if(a[x]) return a[x];
+	if(!x)return 0;
+	if(a[x])return a[x];
 	else return a[x]=f(x-1)+f(x-2);
 }
 int main(){

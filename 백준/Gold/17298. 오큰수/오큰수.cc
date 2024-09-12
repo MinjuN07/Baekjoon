c9c9e77999983dce8c4a9bfd6e411a4c8cd21973
2minJun <127070837+MinjuN07@users.noreply.github.com>
2024-09-12 17:40:30 +0900

    [Gold IV] Title: 오큰수, Time: 196 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.cc
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -1,9 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<stack>
-
 using namespace std;
-
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL); cout.tie(NULL);
@@ -12,17 +10,14 @@ int main(){
   vector<int> arr(n);
   vector<int> ans(n, -1);
   stack<int> s;
-
-  for(int i = 0; i < n; i++) cin >> arr[i];
-
   for(int i=0;i<n;i++) {
+    cin >> arr[i];
     while(!s.empty() && arr[s.top()] < arr[i]) {
       ans[s.top()] = arr[i];
       s.pop();
     }
     s.push(i);
   }
-  
   for(int i=0;i<n;i++) cout << ans[i] << ' ';
   return 0;
 }

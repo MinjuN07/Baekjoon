e989cf97a3fd04ecd0edf9cff117aca2d0c5f808
2minJun <127070837+MinjuN07@users.noreply.github.com>
2024-09-12 15:40:02 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1500 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -1,10 +1,18 @@
-#include<stdio.h>
-int i,j,a[10001];
-int main(){
-	scanf("%d",&j);
-	while(~scanf("%d",&i)) a[i]++;
-	for(i=0;i<10001;i++){
-		if(!a[i]) continue;
-		for(j=0;j<a[i];j++) printf("%d\n",i);
-	}
+#include <iostream>
+using namespace std;
+int i, j, a[10001];
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin >> j;
+    while (cin >> i) {
+        a[i]++;
+    }
+    for (i = 0; i < 10001; i++) {
+        if (!a[i]) continue;
+        for (j = 0; j < a[i]; j++) {
+            cout << i << '\n';
+        }
+    }
+    return 0;
 }
